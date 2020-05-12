e322ac7153bd64e4b0379461c4057995206d90f0
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-12 19:28:24 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Aa = new System.Windows.Forms.TextBox();
+            this.Bb = new System.Windows.Forms.TextBox();
+            this.Hh = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rez = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Aa
+            // 
+            this.Aa.Location = new System.Drawing.Point(61, 37);
+            this.Aa.Name = "Aa";
+            this.Aa.Size = new System.Drawing.Size(100, 22);
+            this.Aa.TabIndex = 0;
+            // 
+            // Bb
+            // 
+            this.Bb.Location = new System.Drawing.Point(61, 79);
+            this.Bb.Name = "Bb";
+            this.Bb.Size = new System.Drawing.Size(100, 22);
+            this.Bb.TabIndex = 1;
+            // 
+            // Hh
+            // 
+            this.Hh.Location = new System.Drawing.Point(61, 121);
+            this.Hh.Name = "Hh";
+            this.Hh.Size = new System.Drawing.Size(100, 22);
+            this.Hh.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "a=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "h=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "b=";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rez
+            // 
+            this.rez.Location = new System.Drawing.Point(200, 37);
+            this.rez.Multiline = true;
+            this.rez.Name = "rez";
+            this.rez.ReadOnly = true;
+            this.rez.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.rez.Size = new System.Drawing.Size(264, 219);
+            this.rez.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 297);
+            this.Controls.Add(this.rez);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Hh);
+            this.Controls.Add(this.Bb);
+            this.Controls.Add(this.Aa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Aa;
+        private System.Windows.Forms.TextBox Bb;
+        private System.Windows.Forms.TextBox Hh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox rez;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double f(double x)
+        {
+            try
+            {
+                if (Math.Abs(Math.Pow(x, 3) + 8) == 0) throw new Exception();
+                else return 3 / (Math.Abs(Math.Pow(x, 3) + 8));
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(Aa.Text);
+                double b = double.Parse(Bb.Text);
+                double h = double.Parse(Hh.Text);
+                if (b < a || h < 0) throw new Exception();
+                for (double i = a; i <= b; i += h)
+                    try
+                    {
+                        rez.Text += "y(";
+                        rez.Text += i;
+                        rez.Text += ") = ";
+                        rez.Text += f(i);
+                        rez.Text += Environment.NewLine;
+                    }
+                    catch
+                    {
+                        rez.Text += "error";
+                        rez.Text += Environment.NewLine;
+                    }
+            }
+            catch (FormatException)
+            {
+                rez.Text += Convert.ToString("Неверный формат ввода данных");
+            }
+            catch
+            {
+                rez.Text += Convert.ToString("Некорректный ввод данных!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Aa.Clear();
+            Bb.Clear();
+            Hh.Clear();
+            rez.Clear();
+        }
+    }
+}
